65fad1ff9f5ad81ea342f922e6102f5ab9d9322b
hibohiboo <swdice66@gmail.com>
2021-07-23 22:26:08 +0900

    add twigテンプレート追加 AB#87

--- public/index.php
+++ public/index.php
@@ -2,14 +2,27 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
 
-$app->get('/', function (Request $request, Response $response, $args) {
+// Create Twig
+$twig = Twig::create('templates', ['cache' => false]);
+$app->add(TwigMiddleware::create($app, $twig));
+
+$app->get('/hello', function (Request $request, Response $response, $args) {
     $response->getBody()->write("Hello world!");
     return $response;
+});
+
+$app->get('/', function ($request, $response, $args) {
+    $view = Twig::fromRequest($request);
+    return $view->render($response, 'index.html', [
+        'name' => 'test name'
+    ]);
 });
 
 $app->run();
